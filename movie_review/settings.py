d809f10cda74ac75440eb88081cb2cbf17bc6f32
irene024081 <irene024081@gmail.com>
2023-04-06 17:12:31 -0400

    add email

--- movie_review/settings.py
+++ movie_review/settings.py
@@ -164,10 +164,10 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'email-smtp.us-east-1.amazonaws.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
-EMAIL_PORT = 2587
+EMAIL_PORT = 587
 EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = "watchmate.prog8420@gmail.com"
+DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER")
